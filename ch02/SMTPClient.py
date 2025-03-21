19e2984afc06e3a7fc2542057a810e9a8a900549
yuanf <yffu@umich.edu>
2025-03-20 17:08:51 -0700

    tried starttls with no luck

--- ch02/SMTPClient.py
+++ ch02/SMTPClient.py
@@ -1,15 +1,46 @@
 import socket
 import ssl
+import base64
 
 class SMTPClient(object):
+    def run1(self):
+        server_name = 'smtp.gmail.com'
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_name, 587))
+        recv = client_socket.recv(1024).decode()
+        print(recv)
+        if recv[:3] != '220':
+            print('220 reply not received from server.')
+        send_cmd = 'HELO tester\r\n'
+        client_socket.send(send_cmd.encode())
+        recv = client_socket.recv(1024).decode()
+        print(recv)
+        if recv[:3] != '250':
+            print('250 reply not received from server.')
+        # Send MAIL FROM command and print server response.
+        # send_cmd = 'MAIL FROM: <test_send@gmail.com>\r\n'
+        client_socket.send('STARTTLS\r\n'.encode())
+        recv = client_socket.recv(1024).decode()
+        print(recv)
+        ssl_sock = context.wrap_socket(client_socket, server_hostname=server_name)
+        ssl_sock.send('EHLO tester\r\n'.encode())
+        data = ssl_sock.recv(1024)
+        print(data.decode())
+        ssl_sock.send('AUTH LOGIN\r\n'.encode())
+        data = ssl_sock.recv(1024)
+        print(data.decode())
+        # https://tedboy.github.io/python_stdlib/generated/generated/smtplib.SMTP.starttls.html
+        # but how to implement using send/recv on the ssl socket?
+
+
     def run(self):
         end_msg = "\r\n.\r\n"
         # Choose a mail server (e.g. Google mail server) and call it mailserver
-        mail_server = 'smtp.freesmtpservers.com'
         # Create socket called clientSocket and establish a TCP connection with mailserver
-        context = ssl.create_default_context()
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((mail_server, 25))
+        client_socket.connect(('smtp.freesmtpservers.com', 25))
+        # client_socket.connect(('smtp.gmail.com', 587))
         recv = client_socket.recv(1024).decode()
         if recv[:3] != '220':
             print('220 reply not received from server.')
@@ -63,3 +94,4 @@ class SMTPClient(object):
 
 if __name__ == '__main__':
     SMTPClient().run()
+    # SMTPClient().run1()
